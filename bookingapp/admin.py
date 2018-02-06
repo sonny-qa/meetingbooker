2d0a731ae98cc9665148b90f6e9df6dc8a2600ea
sonny parsonage <sonnyparsonage@gmail.com>
2018-02-06 20:38:24 +0000

    added models

--- bookingapp/admin.py
+++ bookingapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Venue, Features, Room, Booking
 # Register your models here.
+
+admin.site.register(Venue)
+admin.site.register(Features)
+admin.site.register(Room)
+admin.site.register(Booking)
--- bookingapp/models.py
+++ bookingapp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
-
+import uuid
+from django.urls import reverse #generate URL by reversing url pattern
+from django.conf import settings
 # Create your models here.
+
+
+class Venue(models.Model):
+	"""
+	Model representing a venue, i.e. a church, mosque etc.
+	"""
+
+	name = models.CharField(max_length=200, help_text="Enter a venue name")
+	address = models.CharField(max_length=200, help_text="Enter the address")
+
+
+	def __str__(self):
+		return self.name
+
+class Features(models.Model):
+	"""
+	Model to represent the different room features, (e.g. wifi, projector etc.)
+	"""
+	name = models.CharField(max_length=200, help_text="Enter a feature")
+
+class Room(models.Model):
+
+	"""
+	Model representing a room within a venue
+	"""
+	name = models.CharField(max_length=200, help_text="Enter a room name")
+	seats = models.IntegerField()
+	features = models.ManyToManyField(Features, help_text="Select room features")
+	venue = models.ForeignKey('Venue',on_delete=models.CASCADE)
+	rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(Self):
+		return self.name
+
+	def get_absolute_url(self):
+		"""
+		returns the url to access the detail of this room
+		"""
+		return reverse('room-detail',args=[str(self.id)])
+
+class Booking(models.Model):
+	"""
+	Model representing a specific booking of a room within a venue
+	each Booking will need a globally unique value - the Id
+	"""
+
+	id = models.UUIDField(primary_key=True,default=uuid.uuid4,help_text="Unique id for this booking for the entire system")
+	start_time = models.DateTimeField(null=True,blank=True)
+	end_time = models.DateTimeField(null=True,blank=True)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+	room = models.ForeignKey('Room',on_delete=models.CASCADE)
+	venue = models.ForeignKey('Venue',on_delete=models.CASCADE)
+
+	def __str__(Self):
+		return self.name
+
+	def get_absolute_url(self):
+		"""
+		returns the url to access the detail of this booking
+		"""
+		return reverse('booking-detail',args=[str(self.id)])
+
