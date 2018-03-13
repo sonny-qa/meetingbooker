a8b597485f46fb36e68dfc3dd93a5424e4ab6cd5
sonny parsonage <sonnyparsonage@gmail.com>
2018-03-13 16:04:37 +0000

    added base templates and views

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Venue, Features, Room, Booking
 # Create your views here.
+
+
+def index(request):
+
+	num_venues = Venue.objects.all().count()
+	num_rooms = Room.objects.all().count()
+
+	return render(
+		request,
+		'index.html',
+		context={'num_venues': num_venues,'num_rooms': num_rooms},
+
+		)
