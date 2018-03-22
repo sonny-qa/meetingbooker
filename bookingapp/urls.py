04cbf31d820130eb7354a9bb73aaf57d6a9a6592
sonny parsonage <sonnyparsonage@gmail.com>
2018-03-22 14:35:55 +0000

    added adit venue page with dropdown

--- bookingapp/urls.py
+++ bookingapp/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('rooms/', views.RoomListView.as_view(), name='rooms'),
 	path('room/<int:pk>', views.RoomDetailView.as_view(), name='room-detail'),
-	path('add-loc/', views.addLoc, name='add-loc'),
-
+	path('venue/edit-all',views.edit_all_venues,name="edit-all-venues"),
 ]
--- bookingapp/views.py
+++ bookingapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Venue, Features, Room, Booking
 from django.views import generic
+from django import forms
+
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.forms import modelformset_factory
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -37,9 +44,32 @@ class RoomListView(generic.ListView):
 class RoomDetailView(generic.DetailView):
     model = Room
 
-def addLoc(request):
+def editVenue(request):
+	#needs to return a list of venues 
+
 	return render(
 		request,
-		'bookingapp/add_loc.html',
+		'bookingapp/edit_venue.html',
 		context={'GOOGLE_API':GOOGLE_API}
 		)
+
+def edit_all_venues(request):
+	if request.method == 'POST':
+		form = VenueSelectForm(request.POST)
+		if form.is_valid():
+			
+			return HttpResponseRedirect(reverse('rooms') )  # does nothing, just trigger the validation
+
+	else:
+		form = VenueSelectForm()
+		return render(request, 'bookingapp/venue_edit_all.html', {'form': form})
+
+
+
+
+class VenueSelectForm(forms.Form):
+
+	venue = forms.ChoiceField(choices=[(v.id, str(v)) for v in Venue.objects.all()]
+		,label="Select a Venue to edit ")
+	name = forms.CharField(max_length=30,label="Venue name")
+	
